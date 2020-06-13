9a7ae7fc86a048765f18d0fcd3c7583a60e74d34
Alex <lightskywrites@gmail.com>
2020-06-13 05:21:42 -0500

    Reordered the ignored items table and removed items that don't exist in Classic.

--- DB/DB_IgnoredItems.lua
+++ DB/DB_IgnoredItems.lua
@@ -3,12 +3,10 @@ local L = addonTbl.L;
 
 -- TODO: Reconsider the approach to ignoring specific items.
 local ignoredItems = {
-	[21218] 		= "Blue Qiraji Resonating Crystal",
-	[141605] 		= "Flight Master's Whistle",
-    [21323] 		= "Green Qiraji Resonating Crystal",
 	[11736] 		= "Libram of Resilience",
-	[137642]		= "Mark of Honor",
+	[21218] 		= "Blue Qiraji Resonating Crystal",
     [21321] 		= "Red Qiraji Resonating Crystal",
+    [21323] 		= "Green Qiraji Resonating Crystal",
     [21324] 		= "Yellow Qiraji Resonating Crystal",
 };
 
